c08d06da45e25010dbd42f6e18f0e6206a2da25b
Opilane TTHK <opilane@tthk.ee>
2019-09-13 18:20:25 +0300

    Operators

--- operators/Program.cs
+++ operators/Program.cs
@@ -9,6 +9,41 @@ namespace operators
     {
         static void Main(string[] args)
         {
+            int x = 5;
+            int y = 2;
+
+            //Arithmetic Operators
+            //Multipe Line Comment
+            /*Console.WriteLine(x + y);
+            Console.WriteLine(x - y);
+            Console.WriteLine(x * y);
+            Console.WriteLine(x / y); */
+
+            //Console.WriteLine(x = y);
+            //Console.WriteLine(x += y);
+            //Console.WriteLine(x -= y);
+            //Console.WriteLine(x *= y);
+            //Console.WriteLine(x % y);
+            //Console.WriteLine(x /= y);
+
+            //Comparison Operators
+            /*Console.WriteLine(x == y);
+            Console.WriteLine(x != y);
+            Console.WriteLine(x > y);
+            Console.WriteLine(x >= y);
+            Console.WriteLine(x < y);
+            Console.WriteLine(x <= y);*/
+
+            //Logical Operators
+            // && - AND 
+            // || - OR (pipes)
+            Console.WriteLine(x != y && y == 2);
+            Console.WriteLine(x != y && y == 10);
+            Console.WriteLine(x != y || y == 2);
+            Console.WriteLine(!(y == 10));
+            
+
+            Console.ReadLine();
         }
     }
 }
